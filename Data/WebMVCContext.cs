773cc75ff7ae5e86bf1a308f88673688bf3896bc
Marcos Ferreira <marcosprofissional15@gmail.com>
2025-07-12 16:31:37 -0300

    CRUD Scaffolding

---
+++ Data/WebMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using shop_asp_net_core_mvc.Models;
+
+namespace shop_asp_net_core_mvc.Data
+{
+    public class WebMVCContext : DbContext
+    {
+        public WebMVCContext (DbContextOptions<WebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<shop_asp_net_core_mvc.Models.Department> Department { get; set; } = default!;
+    }
+}
---
+++ Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace shop_asp_net_core_mvc.Models.ViewModels;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
